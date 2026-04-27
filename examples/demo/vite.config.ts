ee803915dd83b54ad6c89e6dbb3df7267e0a7b7d
Jan Nicklas <j.nicklas@me.com>
2026-04-27 09:56:20 +0200

    add github pages

--- examples/demo/vite.config.ts
+++ examples/demo/vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/mini-react-repl/' : '/',
   plugins: [react()],
   server: {
     port: 5173,
@@ -15,4 +16,4 @@ export default defineConfig({
   worker: {
     format: 'es',
   },
-});
+}));
